7917e68328bd69a28102c71e481fc31be7d480e3
JoshAshby <joshuaashby@joshashby.com>
2013-08-25 11:23:10 -0600

    single image views too

---
+++ app/controllers/screenshots/view.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+
+For more information, see: https://github.com/JoshAshby/
+
+http://xkcd.com/353/
+
+Josh Ashby
+2013
+http://joshashby.com
+joshuaashby@joshashby.com
+"""
+from seshat.route import autoRoute
+from seshat.baseObject import HTMLObject
+
+
+@autoRoute()
+class view(HTMLObject):
+    """
+    Returns base index page listing all gifs
+    """
+    _title = "photo"
+    _defaultTmpl = "public/screenshots/view"
+    def GET(self):
+        """
+        """
+        raw = self.request.id.rsplit(".", 1)
+        if len(raw) > 1:
+            name = raw[0].replace("_", " ")
+            self.view.data = {"picture": self.request.id, "name": name}
+        else:
+            pass
+        return self.view.render()
