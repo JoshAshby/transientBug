c392168ae6c4dd507cc2b48ab3d6de750bf9b0ed
JoshAshby <joshuaashby@joshashby.com>
2013-12-25 11:08:22 -0700

    moving notes to new sidebar layout
    going to finally kill the old one soon

--- interface/static/js/transientbug/note.js
+++ interface/static/js/transientbug/note.js
@@ -13,29 +13,6 @@ $(function() {
     };
   });
 
-  $("#save_btn").click(function() {
-    $("#edit_form").submit();
-  });
-
-  $("a").click(function() {
-    var wat = $(this).data("toggle");
-
-    if(wat) {
-      $(".inset").hide();
-      $("#"+wat).show();
-    };
-  });
-
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("active");
-  });
-
-  $(".sidebar-nav").find('a[href="#"]').click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("active");
-  });
-
   var editor = new EpicEditor({
     theme: {
       base: 'css/lib/epic/base/epiceditor.css',
@@ -43,32 +20,15 @@ $(function() {
       editor: 'css/lib/epic/editor/epic-dark.css'
     },
     basePath: '/static/',
-    autogrow: true,
+    autogrow: {
+      minHeight: 200
+    },
     textarea: "content"
   }).load();
 
   editor.on('update', function () {
     $("#preview").html(this.exportFile(null, 'html'));
   }).emit('update');
-
-  //setup before functions
-  var typingTimer;                //timer identifier
-  var doneTypingInterval = 5000;  //time in ms, 5 second for example
-
-  //on keyup, start the countdown
-  $('#title').keyup(function(){
-      typingTimer = setTimeout(doneTyping, doneTypingInterval);
-  });
-
-  //on keydown, clear the countdown 
-  $('#title').keydown(function(){
-      clearTimeout(typingTimer);
-  });
-
-  //user is "finished typing," do something
-  function doneTyping () {
-    $("#title_header").html($("#title").val());
-  };
 
   $("#tags").pillbox();
 
