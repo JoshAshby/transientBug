dd12765b8cb87abae1f3dd07762e043805a2da99
JoshAshby <joshuaashby@joshashby.com>
2014-02-01 18:41:28 -0700

    GRUNT rework all static files to make grunt a breeze
    srsly, grunt.js is so cool
    May have to make more advanced targets later, but for now this works. Builds
    the final statics dir in interface/build.
    just cp -a interface/build /www/static
    or whatever

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      compile:  {
+        expand: true,     // Enable dynamic expansion.
+        cwd: 'interface/src/coffee',      // Src matches are relative to this path.
+        src: ['**/*.coffee'], // Actual pattern(s) to match.
+        dest: 'interface/build/js',   // Destination path prefix.
+        ext: '.js',   // Dest filepaths will have this extension.
+      }
+    },
+    less: {
+      compile:  {
+        expand: true,     // Enable dynamic expansion.
+        cwd: 'interface/src/less/',      // Src matches are relative to this path.
+        src: [
+          'cyti/*.less',
+          'sidebars/*.less',
+          'themes/*.less',
+          'slideshows/*.less',
+          '*.less',
+        ],
+        dest: 'interface/build/css',   // Destination path prefix.
+        ext: '.css',   // Dest filepaths will have this extension.
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path and its sub-directories
+          {
+            expand: true,
+            cwd: 'interface/lib/',      // Src matches are relative to this path.
+            src: [
+              '**'
+            ],
+            dest: 'interface/build/'
+          },
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['coffee', 'less', 'copy']);
+
+};
