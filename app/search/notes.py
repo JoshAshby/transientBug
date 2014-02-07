5e1bf547569a8e442b211c848d406ee89ca51de6
JoshAshby <joshuaashby@joshashby.com>
2014-02-07 14:42:15 -0700

    bad practices with my own code. update add function for note indexer

--- app/search/notes.py
+++ app/search/notes.py
@@ -2,6 +2,8 @@ import search.base_search as searcher
 from whoosh.fields import SchemaClass, TEXT, KEYWORD, ID, DATETIME, BOOLEAN
 from whoosh.analysis import FancyAnalyzer
 
+import copy
+
 
 class NoteSchema(SchemaClass):
     id = ID(stored=True, unique=True)
@@ -20,6 +22,10 @@ class NoteSearcher(searcher.BaseSearcher):
     name = "notes"
 
     def add(self, note):
-        self.writer.add_document(
-
-        )
+      # This isn't at all anything near best practice, but it's my own module
+      # and code so I'm allowing myself to do this. I wouldn't recommend it
+      # however for anyone else to do.
+        d = copy.copy(note._data) # BAD BAD BAD BAD
+        tags = d.pop("tags")
+        d["tags"] = ",".join(tags)
+        self.writer.add_document(**d)
