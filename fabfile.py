5bbd4c34fa4bc96efe4a08f1fc031f2e3245420d
JoshAshby <joshuaashby@joshashby.com>
2014-02-09 22:54:08 -0700

    update fab for index rebuild support

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,7 @@
-import os
 import secrets
 from fabric.api import run, env, cd, prefix
 from contextlib import contextmanager as _contextmanager
 
-
-#if env.host == "localhost":
-    #path = os.path.abspath(__file__).rsplit('/', 1)[0]
-    #venv = path + "/virt/{}/bin/activate".format("package")
-    #env.static = "/srv/http/transientbug/static/"
-
-#else:
 path = secrets.remote_base
 env.key_name = secrets.ssh_key
 venv = path + "/virt/{}/bin/activate".format("normal")
@@ -36,6 +28,11 @@ def static():
     with cd(env.path):
         run("grunt")
         run("cp -r interface/build/* {}".format(env.static))
+
+
+def indexes():
+    with virtualenv():
+        run("python rebuild_index.py")
 
 
 def start():
